19be85d8ca116c27c8966dadb16e498686112fa9
Michael Meier <mmeier61@yahoo.com>
2016-01-25 16:47:22 -0600

    Done

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -1,7 +1,7 @@
 # This method takes a string like `"4122226644"` and
 # returns a properly formatted phone number.
 
-def format_phone_number (phone_number_str)
+def format_phone_number(phone_number_str)
   phone_number_str = phone_number_str.delete('^0-9')
   phone_number_str.insert(0,'(').insert(4,')').insert(5,' ').insert(9, '-')
 end
